4d2cb95357026ef9edb0a8bfd5e86c3ed8b818c1
Hopper Gee <hopper.gee@hey.com>
2021-09-27 00:56:25 +0800

    Refactor belongs_to_tenant to setup default_scope and unscope_tenant

--- lib/multi_tenant_support/concern/model_concern.rb
+++ lib/multi_tenant_support/concern/model_concern.rb
@@ -5,7 +5,24 @@ module MultiTenantSupport
     class_methods do
 
       def belongs_to_tenant(name, **options)
+        options[:foreign_key] ||= MultiTenantSupport.configuration.foreign_key
         belongs_to name.to_sym, **options
+
+        set_default_scope_under_current_tenant(options[:foreign_key])
+      end
+
+      private
+
+      def set_default_scope_under_current_tenant(foreign_key)
+        default_scope lambda {
+          raise "Current tenant is missing" unless MultiTenantSupport.current_tenant
+
+          tenant_account_primary_key = MultiTenantSupport.configuration.primary_key
+          tenant_account_id = MultiTenantSupport.current_tenant.send(tenant_account_primary_key)
+          where(foreign_key => tenant_account_id)
+        }
+
+        scope :unscope_tenant, -> { unscope(where: foreign_key) }
       end
 
     end
--- test/multi_tenant_support/concern/model_concern_test.rb
+++ test/multi_tenant_support/concern/model_concern_test.rb
@@ -8,4 +8,25 @@ class MultiTenantSupport::ModelConcernTest < ActiveSupport::TestCase
     assert_equal accounts(:kohler), users(:robin).account
   end
 
+  test '.belongs_to_tenant - set default scope to under current tenant' do
+    MultiTenantSupport.under_tenant accounts(:beer_stark) do
+      assert_equal 3, User.unscope_tenant.count
+      assert_equal 1, User.count
+      assert_equal users(:jack), User.first
+      assert_equal users(:jack), User.last
+      kate = User.new(name: 'kate')
+      assert kate.save
+      assert_equal kate, User.where(name: 'kate').first
+    end
+  end
+
+  test '.belongs_to_tenant - raise error when tenant is missing' do
+    assert_raise("Current tenant is missing") { User.count }
+    assert_raise("Current tenant is missing") { User.first }
+    assert_raise("Current tenant is missing") { User.last }
+    assert_raise("Current tenant is missing") { User.new }
+    assert_raise("Current tenant is missing") { User.create(email: 'test@test.com') }
+    assert_raise("Current tenant is missing") { User.where(name: 'jack') }
+  end
+
 end
