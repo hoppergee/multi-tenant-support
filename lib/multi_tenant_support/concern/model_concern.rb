d551b48352b39bf2403de4857beb148ba51714d9
Hopper Gee <hopper.gee@hey.com>
2021-09-27 20:38:54 +0800

    Make default scope working when .default_scope_on? is false

--- lib/multi_tenant_support/concern/model_concern.rb
+++ lib/multi_tenant_support/concern/model_concern.rb
@@ -15,11 +15,15 @@ module MultiTenantSupport
 
       def set_default_scope_under_current_tenant(foreign_key)
         default_scope lambda {
-          raise "Current tenant is missing" unless MultiTenantSupport.current_tenant
+          if MultiTenantSupport.default_scope_on?
+            raise "Current tenant is missing" unless MultiTenantSupport.current_tenant
 
-          tenant_account_primary_key = MultiTenantSupport.configuration.primary_key
-          tenant_account_id = MultiTenantSupport.current_tenant.send(tenant_account_primary_key)
-          where(foreign_key => tenant_account_id)
+            tenant_account_primary_key = MultiTenantSupport.configuration.primary_key
+            tenant_account_id = MultiTenantSupport.current_tenant.send(tenant_account_primary_key)
+            where(foreign_key => tenant_account_id)
+          else
+            where(nil)
+          end
         }
 
         scope :unscope_tenant, -> { unscope(where: foreign_key) }
--- test/multi_tenant_support/concern/model_concern_test.rb
+++ test/multi_tenant_support/concern/model_concern_test.rb
@@ -8,25 +8,34 @@ class MultiTenantSupport::ModelConcernTest < ActiveSupport::TestCase
     assert_equal accounts(:kohler), users(:robin).account
   end
 
-  test '.belongs_to_tenant - set default scope to under current tenant' do
-    MultiTenantSupport.under_tenant accounts(:beer_stark) do
-      assert_equal 3, User.unscope_tenant.count
-      assert_equal 1, User.count
-      assert_equal users(:jack), User.first
-      assert_equal users(:jack), User.last
-      kate = User.new(name: 'kate')
-      assert kate.save
-      assert_equal kate, User.where(name: 'kate').first
+  test '.belongs_to_tenant - set default scope to under current tenant when default scope is on' do
+    MultiTenantSupport.turn_default_scope_on do
+      MultiTenantSupport.under_tenant accounts(:beer_stark) do
+        assert_equal 1, User.count
+        assert_equal users(:jack), User.first
+        assert_equal users(:jack), User.last
+        kate = User.new(name: 'kate')
+        assert kate.save
+        assert_equal kate, User.where(name: 'kate').first
+      end
     end
   end
 
-  test '.belongs_to_tenant - raise error when tenant is missing' do
-    assert_raise("Current tenant is missing") { User.count }
-    assert_raise("Current tenant is missing") { User.first }
-    assert_raise("Current tenant is missing") { User.last }
-    assert_raise("Current tenant is missing") { User.new }
-    assert_raise("Current tenant is missing") { User.create(email: 'test@test.com') }
-    assert_raise("Current tenant is missing") { User.where(name: 'jack') }
+  test '.belongs_to_tenant - raise error when tenant is missing and default scope on' do
+    MultiTenantSupport.turn_default_scope_on do
+      assert_raise("Current tenant is missing") { User.count }
+      assert_raise("Current tenant is missing") { User.first }
+      assert_raise("Current tenant is missing") { User.last }
+      assert_raise("Current tenant is missing") { User.new }
+      assert_raise("Current tenant is missing") { User.create(email: 'test@test.com') }
+      assert_raise("Current tenant is missing") { User.where(name: 'jack') }
+    end
+  end
+
+  test ".belongs_to_tenant - won't raise error when tenant is missing but default scope off" do
+    MultiTenantSupport.turn_default_scope_off do
+      assert_equal 3, User.count
+    end
   end
 
 end
