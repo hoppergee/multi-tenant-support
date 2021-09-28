bb7c9282468b021b6b110dae74002c299e4698c1
Hopper Gee <hopper.gee@hey.com>
2021-09-28 22:30:14 +0800

    Create default scope setup test

---
+++ test/multi_tenant_support/concern/model_concern/belongs_to_tenant/default_scope_setup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class MultiTenantSupport::ModelConcern::BelongsToTenant_DefaultScopeSetupTest < ActiveSupport::TestCase
+
+  setup do
+    MultiTenantSupport.turn_default_scope_on
+  end
+
+  test 'set default scope to under current tenant' do
+    MultiTenantSupport.under_tenant accounts(:amazon) do
+      assert_equal 1, User.all.to_a.count
+      assert_equal 1, User.count
+      assert_equal users(:bezos), User.first
+      assert_equal users(:bezos), User.last
+      assert_equal users(:bezos), User.find_by(name: 'Jeff Bezos')
+      assert_equal users(:bezos), User.where(name: 'Jeff Bezos').first
+      kate = User.new(name: 'kate')
+      assert kate.save
+      assert_equal kate, User.where(name: 'kate').first
+    end
+  end
+
+  test 'raise error when tenant is missing' do
+    assert_raise(MultiTenantSupport::MissingTenantError) { User.all }
+    assert_raise(MultiTenantSupport::MissingTenantError) { User.count }
+    assert_raise(MultiTenantSupport::MissingTenantError) { User.first }
+    assert_raise(MultiTenantSupport::MissingTenantError) { User.last }
+    assert_raise(MultiTenantSupport::MissingTenantError) { User.new }
+    assert_raise(MultiTenantSupport::MissingTenantError) { User.create(email: 'test@test.com') }
+    assert_raise(MultiTenantSupport::MissingTenantError) { User.where(name: 'bezos') }
+    assert_raise(MultiTenantSupport::MissingTenantError) { User.find_by(name: 'bezos') }
+  end
+
+end
