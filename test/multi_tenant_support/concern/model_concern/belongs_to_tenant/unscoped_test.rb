e9e558aa1e3b19326c5289ba2989c2d9a3b33130
Hopper Gee <hopper.gee@hey.com>
2021-09-28 22:31:09 +0800

    Create unscoped test

---
+++ test/multi_tenant_support/concern/model_concern/belongs_to_tenant/unscoped_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class MultiTenantSupport::ModelConcern::BelongsToTenant_UnscopedTest < ActiveSupport::TestCase
+
+  test ".unscoped - raise error when disallow read across tenant (default)" do
+    MultiTenantSupport.under_tenant accounts(:amazon) do
+      assert_raise(MultiTenantSupport::InvalidTenantAccess) { User.unscoped.all.to_a }
+    end
+  end
+
+  test ".unscoped - won't raise error when allow read across tenant" do
+    MultiTenantSupport.turn_default_scope_off do
+      MultiTenantSupport.under_tenant accounts(:amazon) do
+        assert_equal 3, User.unscoped.all.to_a.count
+      end
+    end
+  end
+
+end
