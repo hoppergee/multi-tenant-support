8accc97ed50b0c0409726c8fef85fb6ad723b2fc
Hopper Gee <hopper.gee@hey.com>
2021-09-29 19:51:09 +0800

    Add tests for .insert and .insert!

--- test/multi_tenant_support/concern/model_concern/belongs_to_tenant/create_test.rb
+++ test/multi_tenant_support/concern/model_concern/belongs_to_tenant/create_test.rb
@@ -3,32 +3,46 @@ require 'test_helper'
 class MultiTenantSupport::ModelConcern::BelongsToTenant_CreateTest < ActiveSupport::TestCase
 
   ####
-  # .create and .create!
+  # .save, .create, .create!, .insert and insert!
   ####
-  test "create - auto set tenant account on creation" do
+  test "create/insert - auto set tenant account on creation" do
     MultiTenantSupport.under_tenant accounts(:amazon) do
       kate = User.new(name: 'kate')
       assert kate.save
       assert_equal accounts(:amazon), kate.account
 
-      john = User.create(name: 'john')
-      assert_equal accounts(:amazon), john.account
+      assert_difference 'User.count', 1 do
+        john = User.create(name: 'john')
+        assert_equal accounts(:amazon), john.account
+      end
 
       jack = User.create!(name: 'jack')
       assert_equal accounts(:amazon), jack.account
+
+      assert_difference 'User.count', 1 do
+        User.insert({name: 'tom', created_at: Time.current, updated_at: Time.current})
+        assert_equal accounts(:amazon), User.find_by(name: 'tom').account
+      end
+
+      assert_difference 'User.count', 1 do
+        User.insert!({name: 'robin', created_at: Time.current, updated_at: Time.current})
+        assert_equal accounts(:amazon), User.find_by(name: 'robin').account
+      end
     end
   end
 
-  test "create - raise error on missing tenant when not allow read across tenant (default)" do
+  test "create/insert - raise error on missing tenant when not allow read across tenant (default)" do
     assert_raise(MultiTenantSupport::MissingTenantError) { User.create(email: 'test@test.com') }
     assert_raise(MultiTenantSupport::MissingTenantError) { User.create!(email: 'test@test.com') }
     assert_raise(MultiTenantSupport::MissingTenantError) do
       u = User.new(email: 'test@test.com')
       u.save
     end
+    assert_raise(MultiTenantSupport::MissingTenantError) { User.insert({email: 'test@test.com'}) }
+    assert_raise(MultiTenantSupport::MissingTenantError) { User.insert!({email: 'test@test.com'}) }
   end
 
-  test "create - raise error on missing tenant when allow read across tenant" do
+  test "create/insert - raise error on missing tenant when allow read across tenant" do
     MultiTenantSupport.allow_read_across_tenant do
       assert_raise(MultiTenantSupport::MissingTenantError) { User.create(email: 'test@test.com') }
       assert_raise(MultiTenantSupport::MissingTenantError) { User.create!(email: 'test@test.com') }
@@ -36,6 +50,8 @@ class MultiTenantSupport::ModelConcern::BelongsToTenant_CreateTest < ActiveSuppo
         u = User.new(email: 'test@test.com')
         u.save
       end
+      assert_raise(MultiTenantSupport::MissingTenantError) { User.insert({email: 'test@test.com'}) }
+      assert_raise(MultiTenantSupport::MissingTenantError) { User.insert!({email: 'test@test.com'}) }
     end
   end
 
