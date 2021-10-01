c23faabbbec9914c7ab584f83c6f4a752ebdc6a1
Hopper Gee <hopper.gee@hey.com>
2021-10-01 16:22:41 +0800

    Change dummy rails app to from sqlite to postgresql
    For fixing below error when testing with Sidekiq and ActiveJob
    
    SQLite3::BusyException: database is locked concurrency

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gemspec
 
 group :development do
   gem 'sqlite3'
+  gem 'pg'
 end
 
 # To use a debugger
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_24_125633) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "subdomain"
