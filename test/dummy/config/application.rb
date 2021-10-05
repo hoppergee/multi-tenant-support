62d3c08904b5aef5ef025cfa8a52d81c8c4d181b
Hopper Gee <hopper.gee@hey.com>
2021-10-05 11:58:51 +0800

    Rename "lib/multi_tenant_support.rb" to "lib/multi-tenant-support.rb"
    If this file name is not keep same with the gem name, then bundler can't autoload this gem

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -5,7 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "multi_tenant_support"
+require "multi-tenant-support"
 
 module Dummy
   class Application < Rails::Application
