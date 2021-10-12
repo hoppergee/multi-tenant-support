006c26fb430ae28b7bfa3b0d240404a8b9c9b141
Hopper Gee <hopper.gee@hey.com>
2021-10-13 01:55:53 +0800

    Add a turn off protection for destroy_all on collection test

--- test/multi_tenant_support/concern/model_concern/belongs_to_tenant/delete/destroy_all_on_collection_test.rb
+++ test/multi_tenant_support/concern/model_concern/belongs_to_tenant/delete/destroy_all_on_collection_test.rb
@@ -13,7 +13,7 @@ class ModelDestroyAllOnCollectionProtectTest < ActiveSupport::TestCase
   end
 
   test "fail to destroy when tenant is missing" do
-    disallow_read_across_tenant do
+    turn_on_full_protection do
       missing_tenant do
         refute_destroy_all MultiTenantSupport::MissingTenantError
       end
@@ -34,13 +34,21 @@ class ModelDestroyAllOnCollectionProtectTest < ActiveSupport::TestCase
     end
   end
 
+  test 'can destroy all records by super admin through manual turn off protection' do
+    within_a_request_of super_admin do
+      turn_off_protection do
+        assert_destroy_all(-3)
+      end
+    end
+  end
+
   def assert_destroy_all(number_change)
     assert_difference "UserTag.unscope_tenant.count", number_change do
       tags(:entrepreneur).users.destroy_all
     end
 
-    allow_read_across_tenant do
-      assert_equal 2, UserTag.unscope_tenant.count
+    as_super_admin do
+      assert_equal (3 + number_change), UserTag.unscope_tenant.count
       assert_equal 3, User.unscope_tenant.count
     end
   end
@@ -48,7 +56,7 @@ class ModelDestroyAllOnCollectionProtectTest < ActiveSupport::TestCase
   def refute_destroy_all(error = nil)
     assert_raise(error) { tags(:entrepreneur).users.destroy_all }
 
-    allow_read_across_tenant do
+    as_super_admin do
       assert_equal 3, User.count
       assert_equal 3, UserTag.count
     end
