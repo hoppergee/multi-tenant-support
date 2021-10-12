918702731b650741ae4b31eaea3a5e1757ab0af9
Hopper Gee <hopper.gee@hey.com>
2021-10-13 01:22:22 +0800

    Add a turn off protection test for save without validate

--- test/multi_tenant_support/concern/model_concern/belongs_to_tenant/update/save_without_validate_test.rb
+++ test/multi_tenant_support/concern/model_concern/belongs_to_tenant/update/save_without_validate_test.rb
@@ -13,7 +13,7 @@ class ModelUpdateBySaveWithoutValidateProtectTest < ActiveSupport::TestCase
   end
 
   test "cannot save when tenant is missing" do
-    disallow_read_across_tenant do
+    turn_on_full_protection do
       missing_tenant do
         refute_save bezos, error: MultiTenantSupport::MissingTenantError
       end
@@ -35,6 +35,14 @@ class ModelUpdateBySaveWithoutValidateProtectTest < ActiveSupport::TestCase
   test 'can save by super admin through manual set current tenant' do
     within_a_request_of super_admin do
       under_tenant amazon do
+        assert_save bezos
+      end
+    end
+  end
+
+  test 'can save by super admin through manual turn off protection' do
+    within_a_request_of super_admin do
+      turn_off_protection do
         assert_save bezos
       end
     end
