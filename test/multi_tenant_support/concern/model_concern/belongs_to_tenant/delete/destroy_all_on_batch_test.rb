d7a5d6892417c9a41ff9f6db2a1715604a4091f9
Hopper Gee <hopper.gee@hey.com>
2021-10-13 01:57:35 +0800

    Add a turn off protection test for destroy_all on batch

--- test/multi_tenant_support/concern/model_concern/belongs_to_tenant/delete/destroy_all_on_batch_test.rb
+++ test/multi_tenant_support/concern/model_concern/belongs_to_tenant/delete/destroy_all_on_batch_test.rb
@@ -13,7 +13,7 @@ class ModelDestroyAllOnBatchProtectTest < ActiveSupport::TestCase
   end
 
   test "fail to destroy when tenant is missing" do
-    disallow_read_across_tenant do
+    turn_on_full_protection do
       missing_tenant do
         refute_destroy_all MultiTenantSupport::MissingTenantError
       end
@@ -34,20 +34,28 @@ class ModelDestroyAllOnBatchProtectTest < ActiveSupport::TestCase
     end
   end
 
+  test 'can destroy scoped records by super admin through manual turn off protection' do
+    within_a_request_of super_admin do
+      turn_off_protection do
+        assert_destroy_all(-3)
+      end
+    end
+  end
+
   def assert_destroy_all(number_change)
     assert_difference "User.unscope_tenant.count", number_change do
       User.in_batches.destroy_all
     end
 
-    allow_read_across_tenant do
-      assert_equal 2, User.unscope_tenant.count
+    as_super_admin do
+      assert_equal (3 + number_change), User.unscope_tenant.count
     end
   end
 
   def refute_destroy_all(error = nil)
     assert_raise(error) { User.in_batches.destroy_all }
 
-    allow_read_across_tenant do
+    as_super_admin do
       assert_equal 3, User.count
     end
   end
